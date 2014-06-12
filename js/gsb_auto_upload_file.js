67649bf147d9ee3665d99e19768ce553695a882a
Alexei Gorobets <asgorobets@gmail.com>
2014-06-12 11:46:39 -0700

    WP-3778: Auto-upload files only if there are no Javascript validation errors.

--- js/gsb_auto_upload_file.js
+++ js/gsb_auto_upload_file.js
@@ -3,10 +3,14 @@
   Drupal.behaviors.gsbAutoUploadFile = {
     attach: function (context) {
       $(context).find('input[type="file"]').once('gsb-auto-upload-file').change(function (event) {
-        $(event.target)
-          .closest('.form-managed-file')
-          .find('.form-submit')
-          .trigger('mousedown');
+        // Perform auto-upload only if we don't have validation errors, otherwise an error will appear and immediately disappear.
+        if ($(event.target).closest('.form-managed-file').find('.file-upload-js-error').length == 0) {
+          // Trigger upload button click.
+          $(event.target)
+            .closest('.form-managed-file')
+            .find('.form-submit')
+            .trigger('mousedown');
+        }
       });
     }
   };
